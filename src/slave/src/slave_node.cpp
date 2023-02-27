0d8f94a64a5fd66d148bb0e008ac2f03ee484c02
Harindu Bandara <harindu@megaverse>
2023-02-27 14:41:09 +0530

    got the slave node to work

--- src/slave/src/slave_node.cpp
+++ src/slave/src/slave_node.cpp
@@ -13,18 +13,18 @@ struct SlaveNode {
     slaveOne = node.subscribe("slaveOne", 1000, &SlaveNode::receiveMsg, this);
   }
 
-  void receiveMsg(const std_msgs::String::ConstPtr& msg) {
+  void receiveMsg(const std_msgs::String::ConstPtr& msg){
     ROS_INFO("SlaveOne heard: [%s]", msg->data.c_str());
-
     std::stringstream ss;
     ss  << "SlaveOne got this message: "<< msg->data ;
     std_msgs::String msgOut;
     msgOut.data = ss.str();
     master.publish(msgOut);
   }
+
+
 };
 
-   
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "master");
